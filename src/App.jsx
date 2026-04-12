2bd3d371405307a873b1543a4bae8e96a8425f58
fireaspectII <smalldangernight@gmail.com>
2026-04-12 23:17:53 +0500

    ygthuji

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Categories from './pages/Categories/Categories'
 import ProductDetail from './pages/ProductDetail/ProductDetail'
-import Carousel from './pages/Home/Carousel'
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("All");
@@ -18,7 +17,6 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/living' element={<Categories searchQuery={searchQuery}/>}/>
         <Route path='/product/:id' element={<ProductDetail/>} />
-        <Route path='/carousel' element={<Carousel/>}/>
       </Routes>
       <Footer />
     </section>
